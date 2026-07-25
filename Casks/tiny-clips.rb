6de9c9db7bf98e9bdf854b1a4a35be8bb54b56bf
jamesmontemagno <1676321+jamesmontemagno@users.noreply.github.com>
2026-07-25 21:27:05 +0000

    Update release metadata for v1.5.3-mac

--- Casks/tiny-clips.rb
+++ Casks/tiny-clips.rb
@@ -1,7 +1,7 @@
 cask "tiny-clips" do
   auto_updates true
-  version "1.5.2"
-  sha256 "bbd777eb2a9ad38d07fc031c499172d5527eb3d8bd4a19d64d413c7dae64f578"
+  version "1.5.3"
+  sha256 "b66215950d8625c24b4f53f8a1baacaacd0d35f04ce3c45c54023ca09cb5d4dc"
 
   url "https://github.com/jamesmontemagno/tiny-clips/releases/download/v#{version}-mac/TinyClips-v#{version}-mac.zip"
   name "TinyClips"
